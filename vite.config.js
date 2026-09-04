2c1fd8a3058308d61768571e4265810e1de32bbf
cleliepradeau <clelie.pradeau@gmail.com>
2026-09-04 16:21:36 +0200

    lll

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react()],
   plugins: [tailwindcss()],
-  base: "",
+  base: "https://cleliepradeau.github.io/react",
 });
