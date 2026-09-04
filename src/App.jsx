a0f50a4871db263a399d001f48ca17544da17b18
cleliepradeau <clelie.pradeau@gmail.com>
2026-09-04 15:50:30 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+
+import User from "./components/User";
+
+
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-    </div>
+    <main className="w-120 m-10 p-5 shadow-xl">
+    <section>
+      <h1 className="text-5xl">Room</h1>
+      <p className="text-gray-400">5 people are around right now</p>
+    </section>
+    
+    <User src="https://i.pravatar.cc/75?img=24" name="Dale Houston" job="software Engineer"/>
+    <User src="https://i.pravatar.cc/75?img=22" name="Madge Murphy" job = "Product Designer"/>
+    <User src="https://i.pravatar.cc/75?img=15" name="Margaret Garner" job = "Writer & Journalist"/>
+    <User src="https://i.pravatar.cc/75?img=6" name="Gertrude Roberts" job = "Fashion Designer"/>
+    <User src="https://i.pravatar.cc/75?img=8" name="Joshua Olson" job = "Photographer"/>
+  
+
+    </main>
   );
 }
 
